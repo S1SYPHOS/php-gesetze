684908945fbaafab471eb43cd7eece88d7828bfc
Martin Folkers <hello@twobrain.io>
2021-11-07 16:34:53 +0100

    Add test 'testValidDriver'

--- tests/GesetzTest.php
+++ tests/GesetzTest.php
@@ -11,40 +11,6 @@ namespace S1SYPHOS\Gesetze\Tests;
 class GesetzTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * Properties
-     */
-
-    /**
-     * @var string
-     */
-    private static $text;
-
-
-    /**
-     * Setup
-     */
-
-    public static function setUpBeforeClass(): void
-    {
-        # Setup
-        # (1) Text
-        # Enforce UTF-8 encoding
-        $text = '<!DOCTYPE html><meta charset="UTF-8">';
-
-        # Insert test string
-        $text .= '<div>';
-        $text .= 'This is a <strong>simple</strong> HTML text.';
-        $text .= 'It contains legal norms, like Art. 12 Abs. 1 GG ..';
-        $text .= '.. or § 433 II BGB!';
-        $text .= 'At the same time, there are invalid ones, like ..';
-        $text .= '§ 1a BGB and § 1 GGGG';
-        $text .= '</div>';
-
-        self::$text = $text;
-    }
-
-
-    /**
      * Tests
      */
 
@@ -102,207 +68,207 @@ class GesetzTest extends \PHPUnit\Framework\TestCase
         $norms = [
             # Section
             '§ 1 BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§ 1a BGB' => [
-                'norm' => '1a',
+                'norm'   => '1a',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§§ 1 BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§§ 1a BGB' => [
-                'norm' => '1a',
+                'norm'   => '1a',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             'Artikel 12 GG' => [
-                'norm' => '12',
+                'norm'   => '12',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'GG',
             ],
             'Artikel 12a GG' => [
-                'norm' => '12a',
+                'norm'   => '12a',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'GG',
             ],
             'Art. 12 GG' => [
-                'norm' => '12',
+                'norm'   => '12',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'GG',
             ],
             'Art. 12a GG' => [
-                'norm' => '12a',
+                'norm'   => '12a',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'GG',
             ],
 
             # Subsection
             '§ 1 Absatz 2 BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '2',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§ 1 Abs. 2 BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '2',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§ 1 II BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => 'II',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
 
             '§ 1 Absatz 2a BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '2a',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§ 1 Abs. 2a BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '2a',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§ 1 IIa BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => 'IIa',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
 
 
             # Sentence
             '§ 1 Satz 1 BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '1',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '1',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§ 1 S. 1 BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '1',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '1',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
 
             # Number
             '§ 1 Nummer 1 BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '1',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '1',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§ 1 Nr. 1 BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '1',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '1',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
 
             # Letter
             '§ 1 litera a BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => 'a',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => 'a',
                 'gesetz' => 'BGB',
             ],
             '§ 1 lit. a BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => 'a',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => 'a',
                 'gesetz' => 'BGB',
             ],
             '§ 1 Buchstabe a BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => 'a',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => 'a',
                 'gesetz' => 'BGB',
             ],
             '§ 1 Buchst. a BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => 'a',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => 'a',
                 'gesetz' => 'BGB',
             ],
 
             # Law
             '§ 1 BGB' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
             '§ 1 SGB V' => [
-                'norm' => '1',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'SGB V',
             ],
         ];
@@ -325,30 +291,29 @@ class GesetzTest extends \PHPUnit\Framework\TestCase
         $norms = [
             # Note: Despite using a section sign instead of 'Art(ikel)', this works!
             '§ 1 GG'   => [
-                'norm' => '1',
+                'match'  => '§ 1 GG',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'GG',
             ],
             '§ 1 BGB' => [
-                'norm' => '1',
+                'match'  => '§ 1 BGB',
+                'norm'   => '1',
                 'absatz' => '',
-                'satz' => '',
-                'nr' => '',
-                'lit' => '',
+                'satz'   => '',
+                'nr'     => '',
+                'lit'    => '',
                 'gesetz' => 'BGB',
             ],
         ];
 
         # Run function
-        foreach ($norms as $full => $meta) {
+        foreach ($norms as $match => $data) {
             # Assert result
-            $this->assertEquals([[
-                'full' => $full,
-                'meta' => $meta,
-            ]], $object->extract($full));
+            $this->assertEquals([$data], $object->extract($match));
         }
     }
 
@@ -379,18 +344,31 @@ class GesetzTest extends \PHPUnit\Framework\TestCase
         # (1) Instance
         $object = new \S1SYPHOS\Gesetze\Gesetz();
 
-        # (2) HTML document
+        # (2) Text
+        # Enforce UTF-8 encoding
+        $text = '<!DOCTYPE html><meta charset="UTF-8">';
+
+        # Insert test string
+        $text .= '<div>';
+        $text .= 'This is a <strong>simple</strong> HTML text.';
+        $text .= 'It contains legal norms, like Art. 12 Abs. 1 GG ..';
+        $text .= '.. or § 433 II BGB!';
+        $text .= 'At the same time, there are invalid ones, like ..';
+        $text .= '§ 1a BGB and § 1 GGGG';
+        $text .= '</div>';
+
+        # (3) HTML document
         $dom = new \DOMDocument;
 
         # Run function
-        @$dom->loadHTML(self::$text);
+        @$dom->loadHTML($text);
         $result = $dom->getElementsByTagName('a');
 
         # Assert result
         $this->assertEquals(0, count($result));
 
         # Run function
-        @$dom->loadHTML($object->linkify(self::$text));
+        @$dom->loadHTML($object->linkify($text));
         $result = $dom->getElementsByTagName('a');
 
         # Assert result
@@ -400,7 +378,7 @@ class GesetzTest extends \PHPUnit\Framework\TestCase
         $object->validate = false;
 
         # Run function
-        @$dom->loadHTML($object->linkify(self::$text));
+        @$dom->loadHTML($object->linkify($text));
         $result = $dom->getElementsByTagName('a');
 
         # Assert result
