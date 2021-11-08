0b8d592b7a552dd972db205b8706ffffe569bd4c
Martin Folkers <hello@twobrain.io>
2021-11-08 08:29:40 +0100

    Add driver for 'buzer.de'
    * Add crawler
    * Add driver
    * Connect everything

--- tests/GesetzTest.php
+++ tests/GesetzTest.php
@@ -21,6 +21,7 @@ class GesetzTest extends \PHPUnit\Framework\TestCase
         $drivers = [
             'gesetze'    => '\S1SYPHOS\Gesetze\Drivers\GesetzeImInternet',
             'dejure'     => '\S1SYPHOS\Gesetze\Drivers\DejureOnline',
+            'buzer'      => '\S1SYPHOS\Gesetze\Drivers\Buzer',
             'lexparency' => '\S1SYPHOS\Gesetze\Drivers\Lexparency',
         ];
 
